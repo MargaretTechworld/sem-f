f36d6ee1c40f0879eac51e842138eabc92bff43a
GhostEsso <lilipitaham@gmail.com>
2025-02-04 13:22:52 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Hero from './components/Hero'
 import NewHereSection from './components/NewHereSection'
 import YoungSection from './components/YoungSection'
+import PastorsBlogSection from './components/PastorsBlogSection'
+import Footer from './components/Footer'
 
 export default function Home() {
   return (
@@ -8,6 +10,8 @@ export default function Home() {
       <Hero />
       <NewHereSection />
       <YoungSection />
+      <PastorsBlogSection />
+      <Footer />
     </main>
   );
 }
